7d48fe6a192b0d83c0730d1ac6981596a6ba9467
Zheng Luo <luozheng03@gmail.com>
2016-07-09 11:37:02 -0400

    Update udprecever printing incoming address by inet_ntoa()

--- Network/UDP/udp-recv.c
+++ Network/UDP/udp-recv.c
@@ -1,8 +1,8 @@
 /*
-        demo-udp-03: udp-recv: a simple udp server
-	receive udp messages
+    udp-recv: a simple udp server
+    receive udp messages
 
-        usage:  udp-recv
+    usage:  udp-recv
 */
 
 #include <stdlib.h>
@@ -18,42 +18,40 @@
 int
 main(int argc, char **argv)
 {
-	struct sockaddr_in myaddr;	/* our address */
-	struct sockaddr_in remaddr;	/* remote address */
-	socklen_t addrlen = sizeof(remaddr);		/* length of addresses */
-	int recvlen;			/* # bytes received */
-	int fd;				/* our socket */
-	unsigned char buf[BUFSIZE];	/* receive buffer */
+    struct sockaddr_in myaddr;    /* our address */
+    struct sockaddr_in remaddr;    /* remote address */
+    socklen_t addrlen = sizeof(remaddr);        /* length of addresses */
+    int recvlen;            /* # bytes received */
+    int fd;                /* our socket */
+    unsigned char buf[BUFSIZE];    /* receive buffer */
 
 
-	/* create a UDP socket */
+    /* create a UDP socket */
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("cannot create socket\n");
+        return 0;
+    }
 
-	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("cannot create socket\n");
-		return 0;
-	}
+    /* bind the socket to any valid IP address and a specific port */
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    myaddr.sin_port = htons(SERVICE_PORT);
 
-	/* bind the socket to any valid IP address and a specific port */
+    if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        perror("bind failed");
+        return 0;
+    }
 
-	memset((char *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(SERVICE_PORT);
-
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		return 0;
-	}
-
-	/* now loop, receiving data and printing what we received */
-	for (;;) {
-		printf("waiting on port %d\n", SERVICE_PORT);
-		recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
-		printf("received %d bytes\n", recvlen);
-		if (recvlen > 0) {
-			buf[recvlen] = 0;
-			printf("received message: \"%s\"\n", buf);
-		}
-	}
-	/* never exits */
+    /* now loop, receiving data and printing what we received */
+    for (;;) {
+        printf("waiting on port %d\n", SERVICE_PORT);
+        recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+        printf("received %d bytes from %s \n", recvlen, inet_ntoa(remaddr.sin_addr));
+        if (recvlen > 0) {
+            buf[recvlen] = 0;
+            printf("received message: \"%s\"\n", buf);
+        }
+    }
+    /* never exits */
 }
