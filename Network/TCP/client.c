9fa67a655c3b7bb6420cb0e58693e138a93768bd
Zheng Luo <luozheng03@gmail.com>
2016-07-09 09:24:10 -0400

    Enhance TCP server to process for multiple client

--- Network/TCP/client.c
+++ Network/TCP/client.c
@@ -1,141 +1,180 @@
 /*
-	echoc: a demo of TCP/IP sockets connect
+    echoc: a demo of TCP/IP sockets connect
 
-	usage:	client [-h serverhost] [-p port]
+    usage:    client [-h serverhost] [-p port]
 */
 
 #include <stdio.h>
-#include <stdlib.h>	/* needed for os x*/
-#include <string.h>	/* for strlen */
+#include <stdlib.h>    /* needed for os x*/
+#include <string.h>    /* for strlen */
 #include <netdb.h>      /* for gethostbyname() */
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #include "port.h"       /* defines default port */
 
+#define BUFSIZE 1024
+
 int conn(char *host, int port);
-void disconn(void);
+void disconn(int fd);
+int doprocessing (int fd);
 
 main(int argc, char **argv)
 {
-	extern char *optarg;
-	extern int optind;
-	int c, err = 0; 
-	char *prompt = 0;
-	int port = SERVICE_PORT;	/* default: whatever is in port.h */
-	char *host = "localhost";	/* default: this host */
-	static char usage[] = 
-	              "usage: %s [-d] [-h serverhost] [-p port]\n";
+    extern char *optarg;
+    extern int optind;
+    int c, err = 0;
+    char *prompt = 0;
+    int port = SERVICE_PORT;    /* default: whatever is in port.h */
+    char *host = "localhost";    /* default: this host */
+    int fd;  /* fd is the file descriptor for the connected socket */
+    static char usage[] =  "usage: %s [-d] [-h serverhost] [-p port]\n";
 
-	while ((c = getopt(argc, argv, "dh:p:")) != -1)
-		switch (c) {
-		case 'h':  /* hostname */
-			host = optarg;
-			break;
-		case 'p':  /* port number */
-			port = atoi(optarg);
-			if (port < 1024 || port > 65535) {
-				fprintf(stderr, "invalid port number: %s\n", optarg);
-				err = 1;
-			}
-			break;
-		case '?':
-			err = 1;
-			break;
-		}
-	if (err || (optind < argc)) {	/* error or extra arguments? */
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	}
+    while ((c = getopt(argc, argv, "dh:p:")) != -1)
+        switch (c) {
+        case 'h':  /* hostname */
+            host = optarg;
+            break;
+        case 'p':  /* port number */
+            port = atoi(optarg);
+            if (port < 1024 || port > 65535) {
+                fprintf(stderr, "invalid port number: %s\n", optarg);
+                err = 1;
+            }
+            break;
+        case '?':
+            err = 1;
+            break;
+        }
+    if (err || (optind < argc)) {    /* error or extra arguments? */
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
 
-	printf("connecting to %s, port %d\n", host, port);
+    printf("connecting to %s, port %d\n", host, port);
 
-	if (!conn(host, port))    /* connect */
-		exit(1);   /* something went wrong */
+    if (!(fd = conn(host, port)))    /* connect */
+        exit(1);   /* something went wrong */
 
-	disconn();    /* disconnect */
-	return 0;
+    while (doprocessing(fd)) {}
+
+    disconn(fd);    /* disconnect */
+    return 0;
 }
-
-int fd;  /* fd is the file descriptor for the connected socket */
 
 /* conn: connect to the service running on host:port */
 /* return 0 on failure, non-zero on success */
 int
 conn(char *host, int port)
 {
-	struct hostent *hp;	/* host information */
-	unsigned int alen;	/* address length when we get the port number */
-	struct sockaddr_in myaddr;	/* our address */
-	struct sockaddr_in servaddr;	/* server address */
+    struct hostent *hp;    /* host information */
+    unsigned int alen;    /* address length when we get the port number */
+    struct sockaddr_in myaddr;    /* our address */
+    struct sockaddr_in servaddr;    /* server address */
+    int fd;
 
-	printf("conn(host=\"%s\", port=\"%d\")\n", host, port);
+    printf("conn(host=\"%s\", port=\"%d\")\n", host, port);
 
-	/* get a tcp/ip socket */
-	/* We do this as we did it for the server */
-	/* request the Internet address protocol */
-	/* and a reliable 2-way byte stream */
+    /* get a tcp/ip socket */
+    /* We do this as we did it for the server */
+    /* request the Internet address protocol */
+    /* and a reliable 2-way byte stream */
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("cannot create socket");
+        return 0;
+    }
 
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("cannot create socket");
-		return 0;
-	}
+    /* bind to an arbitrary return address */
+    /* because this is the client side, we don't care about the */
+    /* address since no application will connect here  --- */
+    /* INADDR_ANY is the IP address and 0 is the socket */
+    /* htonl converts a long integer (e.g. address) to a network */
+    /* representation (agreed-upon byte ordering */
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    myaddr.sin_port = htons(0);
 
-	/* bind to an arbitrary return address */
-	/* because this is the client side, we don't care about the */
-	/* address since no application will connect here  --- */
-	/* INADDR_ANY is the IP address and 0 is the socket */
-	/* htonl converts a long integer (e.g. address) to a network */
-	/* representation (agreed-upon byte ordering */
+    if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        perror("bind failed");
+        return 0;
+    }
 
-	memset((char *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(0);
-
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		return 0;
-	}
-
-	/* this part is for debugging only - get the port # that the operating */
-	/* system allocated for us. */
+    /* this part is for debugging only - get the port # that the operating */
+    /* system allocated for us. */
         alen = sizeof(myaddr);
         if (getsockname(fd, (struct sockaddr *)&myaddr, &alen) < 0) {
                 perror("getsockname failed");
                 return 0;
         }
-	printf("local port number = %d\n", ntohs(myaddr.sin_port));
+    printf("local port number = %d\n", ntohs(myaddr.sin_port));
 
-	/* fill in the server's address and data */
-	/* htons() converts a short integer to a network representation */
+    /* fill in the server's address and data */
+    /* htons() converts a short integer to a network representation */
+    memset((char*)&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
 
-	memset((char*)&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
+    /* look up the address of the server given its name */
+    hp = gethostbyname(host);
+    if (!hp) {
+        fprintf(stderr, "could not obtain address of %s\n", host);
+        return 0;
+    }
 
-	/* look up the address of the server given its name */
-	hp = gethostbyname(host);
-	if (!hp) {
-		fprintf(stderr, "could not obtain address of %s\n", host);
-		return 0;
-	}
+    /* put the host's address into the server address structure */
+    memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
 
-	/* put the host's address into the server address structure */
-	memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
+    /* connect to server */
+    if (connect(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("connect failed");
+        return 0;
+    }
+    return fd;
+}
 
-	/* connect to server */
-	if (connect(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-		perror("connect failed");
-		return 0;
-	}
-	return 1;
+/* read data from client and send data to server */
+int
+doprocessing (int fd)
+{
+    int n; /* message byte size */
+    char buf[BUFSIZE];
+
+    /* get message line from the user */
+    printf("Please enter msg: ");
+    bzero(buf, BUFSIZE);
+    fgets(buf, BUFSIZE, stdin);
+
+    if (strncmp(buf, "exit", 4) == 0) return 0;
+    /*
+    One important thing to keep in mind is that TCP/IP
+    sockets give you a byte stream, not a packet stream.
+    If you write 20 bytes to a socket, the other side is not
+    guaranteed to read 20 bytes in a read operation.
+    It may read 20. It may read less if there was some packet fragmentation.
+    In that case, you'll need to loop back and keep reading.
+    If you write 20 bytes to a socket and then write another 20 bytes to a socket,
+    the other side may read all 40 bytes at once ... or not.
+    */
+    /* send the message line to the server */
+    n = write(fd, buf, strlen(buf));
+    if (n < 0)
+      error("ERROR writing to socket");
+
+    /* print the server's reply */
+    bzero(buf, BUFSIZE);
+    n = read(fd, buf, BUFSIZE);
+    if (n < 0)
+      error("ERROR reading from socket");
+    printf("Echo from server: %s", buf);
+
+    return 1;
 }
 
 /* disconnect from the service */
 void
-disconn(void)
+disconn(int fd)
 {
-	printf("disconn()\n");
-	shutdown(fd, 2);    /* 2 means future sends & receives are disallowed */
+    printf("disconn()\n");
+    shutdown(fd, 2);    /* 2 means future sends & receives are disallowed */
 }
--- Network/TCP/server.c
+++ Network/TCP/server.c
@@ -1,16 +1,16 @@
 /*
-	echoserver: TCP/IP sockets example
-	get a connect: keep reading data from the socket, echoing
-	back the received data.
+    echoserver: TCP/IP sockets example
+    get a connect: keep reading data from the socket, echoing
+    back the received data.
 
-	usage:	echoserver [-d] [-p port]
+    usage:    echoserver [-d] [-p port]
 */
 
 #include <stdio.h>
-#include <stdlib.h>	/* needed for os x */
-#include <string.h>	/* for memset */
+#include <stdlib.h>    /* needed for os x */
+#include <string.h>    /* for memset */
 #include <sys/socket.h>
-#include <arpa/inet.h>	/* for inet_ntoa */
+#include <arpa/inet.h>    /* for inet_ntoa */
 #include <netinet/in.h>
 #include <sys/errno.h>   /* defines ERESTART, EINTR */
 #include <sys/wait.h>    /* defines WNOHANG, for wait() */
@@ -21,116 +21,172 @@
 #define ERESTART EINTR
 #endif
 
+#define BUFSIZE 1024
+
 extern int errno;
 
-void serve(int port);	/* main server function */
+void serve(int port);    /* main server function */
 void disconn(void);
+int doprocessing (int rqst);
 
 main(int argc, char **argv)
 {
-	extern char *optarg;
-	extern int optind;
-	int c, err = 0; 
-	int port = SERVICE_PORT;
-	static char usage[] = "usage: %s [-d] [-p port]\n";
+    extern char *optarg;
+    extern int optind;
+    int c, err = 0;
+    int port = SERVICE_PORT;
+    static char usage[] = "usage: %s [-d] [-p port]\n";
 
-	while ((c = getopt(argc, argv, "dp:")) != -1)
-		switch (c) {
-		case 'p':
-			port = atoi(optarg);
-			/* if (port < 1024 || port > 65535) { */
-			if (port < 0 || port > 65535) {
-				fprintf(stderr, "invalid port number: %s\n", optarg);
-				err = 1;
-			}
-			break;
-		case '?':
-			err = 1;
-			break;
-		}
-	if (err || (optind < argc)) {
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	}
-	serve(port);
+    while ((c = getopt(argc, argv, "dp:")) != -1)
+        switch (c) {
+        case 'p':
+            port = atoi(optarg);
+            /* if (port < 1024 || port > 65535) { */
+            if (port < 0 || port > 65535) {
+                fprintf(stderr, "invalid port number: %s\n", optarg);
+                err = 1;
+            }
+            break;
+        case '?':
+            err = 1;
+            break;
+        }
+    if (err || (optind < argc)) {
+        fprintf(stderr, usage, argv[0]);
+        exit(1);
+    }
+    serve(port);
 }
 
 /* serve: set up the service */
-
 void
 serve(int port)
 {
-	int svc;        /* listening socket providing service */
-	int rqst;       /* socket accepting the request */
-	socklen_t alen;       /* length of address structure */
-	struct sockaddr_in my_addr;    /* address of this service */
-	struct sockaddr_in client_addr;  /* client's address */
-	int sockoptval = 1;
-	char hostname[128]; /* host name, for debugging */
+    int svc;        /* listening socket providing service */
+    int rqst;       /* socket accepting the request */
+    socklen_t alen;       /* length of address structure */
+    struct sockaddr_in my_addr;    /* address of this service */
+    struct sockaddr_in client_addr;  /* client's address */
+    int sockoptval = 1;
+    char hostname[128]; /* host name, for debugging */
+    int n; /* message byte size */
+    char buf[BUFSIZE];
 
-	gethostname(hostname, 128);
+    gethostname(hostname, 128);
 
-	/* get a tcp/ip socket */
-	/*   AF_INET is the Internet address (protocol) family  */
-	/*   with SOCK_STREAM we ask for a sequenced, reliable, two-way */
-	/*   conenction based on byte streams.  With IP, this means that */
-	/*   TCP will be used */
+    /* get a tcp/ip socket */
+    /*   AF_INET is the Internet address (protocol) family  */
+    /*   with SOCK_STREAM we ask for a sequenced, reliable, two-way */
+    /*   conenction based on byte streams.  With IP, this means that */
+    /*   TCP will be used */
+    if ((svc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("cannot create socket");
+        exit(1);
+    }
 
-	if ((svc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("cannot create socket");
-		exit(1);
-	}
+    /* we use setsockopt to set SO_REUSEADDR. This allows us */
+    /* to reuse the port immediately as soon as the service exits. */
+    /* Some operating systems will not allow immediate reuse */
+    /* on the chance that some packets may still be en route */
+    /* to the port. */
+    setsockopt(svc, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
 
-	/* we use setsockopt to set SO_REUSEADDR. This allows us */
-	/* to reuse the port immediately as soon as the service exits. */
-	/* Some operating systems will not allow immediate reuse */
-	/* on the chance that some packets may still be en route */
-	/* to the port. */
+    /* set up our address */
+    /* htons converts a short integer into the network representation */
+    /* htonl converts a long integer into the network representation */
+    /* INADDR_ANY is the special IP address 0.0.0.0 which binds the */
+    /* transport endpoint to all IP addresses on the machine. */
+    memset((char*)&my_addr, 0, sizeof(my_addr));  /* 0 out the structure */
+    my_addr.sin_family = AF_INET;   /* address family */
+    my_addr.sin_port = htons(port);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	setsockopt(svc, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
+    /* bind to the address to which the service will be offered */
+    if (bind(svc, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+        perror("bind failed");
+        exit(1);
+    }
 
-	/* set up our address */
-	/* htons converts a short integer into the network representation */
-	/* htonl converts a long integer into the network representation */
-	/* INADDR_ANY is the special IP address 0.0.0.0 which binds the */
-	/* transport endpoint to all IP addresses on the machine. */
+    /* set up the socket for listening with a queue length of 5 */
+    if (listen(svc, 5) < 0) {
+        perror("listen failed");
+        exit(1);
+    }
 
-	memset((char*)&my_addr, 0, sizeof(my_addr));  /* 0 out the structure */
-	my_addr.sin_family = AF_INET;   /* address family */
-	my_addr.sin_port = htons(port);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    printf("server started on %s, listening on port %d\n", hostname, port);
 
-	/* bind to the address to which the service will be offered */
-	if (bind(svc, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
-		perror("bind failed");
-		exit(1);
-	}
+    /* loop forever - wait for connection requests and perform the service */
+    alen = sizeof(client_addr);     /* length of address */
 
-	/* set up the socket for listening with a queue length of 5 */
-	if (listen(svc, 5) < 0) {
-		perror("listen failed");
-		exit(1);
-	}
+    while (1) {
+        rqst = accept(svc, (struct sockaddr *)&client_addr, &alen);
 
-	printf("server started on %s, listening on port %d\n", hostname, port);
+        if (rqst < 0) {
+          perror("ERROR on accept");
+          exit(1);
+        }
 
-	/* loop forever - wait for connection requests and perform the service */
-	alen = sizeof(client_addr);     /* length of address */
+        printf("received a connection from: %s port %d\n",
+            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-	for (;;) {
-		while ((rqst = accept(svc,
-		                (struct sockaddr *)&client_addr, &alen)) < 0) {
-			/* we may break out of accept if the system call */
-			/* was interrupted. In this case, loop back and */
-			/* try again */
-			if ((errno != ECHILD) && (errno != ERESTART) && (errno != EINTR)) {
-				perror("accept failed");
-				exit(1);
-			}
-		}
+        if (rqst < 0) {
+          perror("ERROR on accept");
+          exit(1);
+        }
 
-		printf("received a connection from: %s port %d\n",
-			inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        	shutdown(rqst, 2);    /* close the connection */
-	}
+        /* Create child process */
+        int pid = fork();
+
+        if (pid < 0) {
+          perror("ERROR on fork");
+          exit(1);
+        }
+
+        if (pid == 0) {
+          /* This is the client process */
+          close(svc);
+          while(doprocessing(rqst)){};
+          exit(0);
+        }
+        else {
+          close(rqst);
+        }
+
+        //shutdown(rqst, 2);    /* close the connection */
+    }
+}
+
+/* read data from server and send data to client */
+int
+doprocessing (int rqst)
+{
+    int n; /* message byte size */
+    char buf[BUFSIZE];
+    /*
+    One important thing to keep in mind is that TCP/IP
+    sockets give you a byte stream, not a packet stream.
+    If you write 20 bytes to a socket, the other side is not
+    guaranteed to read 20 bytes in a read operation.
+    It may read 20. It may read less if there was some packet fragmentation.
+    In that case, you'll need to loop back and keep reading.
+    If you write 20 bytes to a socket and then write another 20 bytes to a socket,
+    the other side may read all 40 bytes at once ... or not.
+    */
+    /*
+     * read: read input string from the client
+     */
+    bzero(buf, BUFSIZE);
+    n = read(rqst, buf, BUFSIZE);
+    if (n < 0)
+      error("ERROR reading from socket");
+    printf("server received %d bytes: %s", n, buf);
+
+    /*
+     * write: echo the input string back to the client
+     */
+    n = write(rqst, buf, strlen(buf));
+    if (n < 0)
+      error("ERROR writing to socket");
+
+    return 1;
 }
