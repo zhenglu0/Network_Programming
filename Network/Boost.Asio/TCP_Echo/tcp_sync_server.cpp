4283eb2e74d9418c1e661e2c95b10de6aba3e56e
Zheng Luo <luozheng03@gmail.com>
2016-07-10 21:51:08 -0400

    Update format

--- Network/Boost.Asio/TCP_Echo/tcp_sync_server.cpp
+++ Network/Boost.Asio/TCP_Echo/tcp_sync_server.cpp
@@ -8,8 +8,7 @@ using namespace boost::asio;
 io_service service;
 typedef boost::system::error_code error_code;
 
-size_t read_complete(char * buff, const error_code & err, size_t
-    bytes) {
+size_t read_complete(char * buff, const error_code & err, size_t bytes) {
     if ( err) return 0;
     bool found = std::find(buff, buff + bytes, '\n') < buff + bytes;
     // we read one-by-one until we get to enter, no buffering
@@ -22,8 +21,7 @@ void handle_connections() {
     while ( true) {
         ip::tcp::socket sock(service);
         acceptor.accept(sock);
-        int bytes = read(sock, buffer(buff),
-            boost::bind(read_complete,buff,_1,_2));
+        int bytes = read(sock, buffer(buff), boost::bind(read_complete,buff,_1,_2));
         std::string msg(buff, bytes);
         sock.write_some(buffer(msg));
         sock.close();
