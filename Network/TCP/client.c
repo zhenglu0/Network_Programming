e4ccec8afdb9180c17d690e5eeffdb5a26f521df
Zheng Luo <luozheng03@gmail.com>
2016-08-20 12:48:32 -0400

    Add handler for testing signal SIGPIPE for client

--- Network/TCP/client.c
+++ Network/TCP/client.c
@@ -12,6 +12,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 #include "port.h"       /* defines default port */
 
@@ -20,6 +21,7 @@
 int conn(char *host, int port);
 void disconn(int fd);
 int doprocessing (int fd);
+void signal_callback_handler(int signum);
 
 int main(int argc, char **argv)
 {
@@ -57,7 +59,10 @@ int main(int argc, char **argv)
 
     if (!(fd = conn(host, port)))    /* connect */
         exit(1);   /* something went wrong */
-
+    
+    /* Catch Signal Handler SIGPIPE */
+    signal(SIGPIPE, signal_callback_handler);
+    
     while (doprocessing(fd)) {}
 
     disconn(fd);    /* disconnect */
@@ -158,16 +163,22 @@ doprocessing (int fd)
     If you write 20 bytes to a socket and then write another 20 bytes to a socket,
     the other side may read all 40 bytes at once ... or not.
     */
+    // http://stackoverflow.com/questions/11436013/writing-to-a-closed-local-tcp-socket-not-failing
     /* send the message line to the server */
+    // it took one write for the sockets to transition to the CLOSED states.
     n = write(fd, buf, strlen(buf));
     if (n < 0)
       perror("ERROR writing to socket");
+    else {
+      printf("server send %d bytes: %s", n, buf);
+    }
 
     /* print the server's reply */
     bzero(buf, BUFSIZE);
-    n = read(fd, buf, BUFSIZE);
+    //n = read(fd, buf, BUFSIZE);
     /* http://www.linuxquestions.org/questions/programming-9/
        how-could-server-detect-closed-client-socket-using-tcp-and-c-824615/ */
+/*
     if (n > 0) {
       printf("Echo from server: %s", buf);
     }
@@ -179,7 +190,7 @@ doprocessing (int fd)
       perror("ERROR reading from socket");
       return 0;
     }
-
+*/
     return 1;
 }
 
@@ -189,4 +200,10 @@ disconn(int fd)
 {
     printf("disconn()\n");
     shutdown(fd, 2);    /* 2 means future sends & receives are disallowed */
+}
+
+/* Catch Signal Handler function */
+void signal_callback_handler(int signum){
+
+        printf("Caught signal SIGPIPE %d\n",signum);
 }
